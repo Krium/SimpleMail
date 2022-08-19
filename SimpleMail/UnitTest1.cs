ee366bdee021f616adb39aabbe9e272ffa08c6e4
Yurii Kryvoshyia <k.yurii02@gmail.com>
2022-08-19 11:01:26 +0300

    changed locators

--- SimpleMail/UnitTest1.cs
+++ SimpleMail/UnitTest1.cs
@@ -10,7 +10,6 @@ namespace SimpleMail
     [TestClass]
     public class UnitTest1
     {
-        private const int timeToWait = 10;
         IWebDriver driver;
         private readonly string email = "testtestovych01082022@gmail.com";
         private readonly string password = "Soncesvityt1822";
@@ -31,7 +30,7 @@ namespace SimpleMail
             WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(3000));
 
 
-            driver.FindElement(By.XPath("//input[@type='email']")).SendKeys(email);
+            driver.FindElement(By.CssSelector("input[type='email']")).SendKeys(email);
             driver.FindElement(By.XPath("//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 qIypjc TrZEUc lw1w4b']")).Click();
 
             wait1.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@type='password']"))).SendKeys(password);
@@ -43,7 +42,7 @@ namespace SimpleMail
             wait1.Until(ExpectedConditions.ElementIsVisible(By.XPath("//textarea[@name='to']"))).SendKeys(testUserEmail);
             wait1.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='Am Al editable LW-avf tS-tW']"))).SendKeys(letterText);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-            wait1.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']"))).Click();
+            wait1.Until(ExpectedConditions.ElementToBeClickable(By.Id(":78"))).Click();
             wait1.Until(ExpectedConditions.ElementIsVisible(By.XPath("//span[@role='link' and @class='ag a8k']"))).Click();
 
         }
